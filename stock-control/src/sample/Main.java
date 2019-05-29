6490acfc2dad1dc40215e1c7813b4b8f01234623
David Spencer <david-spencer@davids-imac.home>
2019-05-29 16:22:00 +0100

    basic JavaFX application

--- stock-control/src/sample/Main.java
+++ stock-control/src/sample/Main.java
@@ -1,19 +1,58 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+/**
+ * The main class of the stock control system builds and displays the application
+ * GUI and initializes all other components.
+ *
+ * @author David Spencer
+ * @version 1.0
+ */
 public class Main extends Application {
+
+    private static Stage stage;
+    private static BorderPane root;
+
+    private Insets padding = new Insets(5, 5, 5, 5);
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+
+        stage = primaryStage;
+
+        Button newProduct = new Button("New Product");
+
+        for (Button button : new Button[] {newProduct}) {
+            button.setMinWidth(200);
+            button.setMaxWidth(200);
+            button.setMinHeight(30);
+            button.setMaxHeight(30);
+            button.setId("topButton");
+        }
+
+        Pane addProduct = new HBox(newProduct);
+        addProduct.setPadding(padding);
+
+        Pane topMenu = new BorderPane(addProduct, null, null, null, null);
+
+        root = new BorderPane(null, topMenu, null, null, null);
+        root.setPrefSize(900, 700);
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("stylesheet.css").toExternalForm());
+
+        stage.setTitle("PlantPact");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
     }
 
 
